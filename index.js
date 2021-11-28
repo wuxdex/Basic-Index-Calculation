1a79f90b388c84322a17b41f552d94f06d5e2b2e
Caner Yıldırım <89223658+wuxdex@users.noreply.github.com>
2021-11-28 21:53:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 let Index;
 
-const weight = 70;
-const height = 1.75;
+const weight = prompt("Kilonuzu Giriniz");
+const height = prompt("Boyunuzu giriniz (1.75)");
 
 Index = (weight) / (height*height);
 console.log(Index)
 
 if (Index < 0){
-    console.log("HATA DEĞERLER SIFIRIN ALTINDA OLAMAZ!")
+    alert("HATA DEĞERLER SIFIRIN ALTINDA OLAMAZ!")
 }
 
 
 if (Index < 18.4 && Index > 0){
-    console.log("Zayıf");
+    alert("Kilo endeksiniz : "+Index + " Zayıfsınız!");
 }
 
 if (Index < 24.9 && Index > 18.5){
-    console.log("Normal");
+    alert("Kilo endeksiniz : "+Index + " Normal");
 }
 
 if (Index < 29.9 && Index > 25){
-    console.log("Kilolu");
+    alert("Kilo endeksiniz : "+Index + " Kilolu");
 }
 
 if (Index < 34.9 && Index > 30){
-    console.log("Obez");
+    alert("Kilo endeksiniz : "+Index + " Obez");
 }
 
 if (Index > 35){
-    console.log('3.derece');
+    alert("Kilo endeksiniz : "+Index + ' 3.dereceden obez');
 }
+
+
